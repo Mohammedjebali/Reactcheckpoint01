9f220d9296fdaf8f7f637d461df78d4509be481c
Mohammedjebali <jebalimohammed7@gmail.com>
2024-09-11 15:30:04 +0100

    reactcheckpoint1

---
+++ src/components/Price.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import product from '../product'
+
+function Price() {
+  return (
+    <div>
+        <h4>{product.price}$</h4>
+    </div>
+  )
+}
+
+export default Price
---
+++ src/product.js
@@ -0,0 +1,7 @@
+const product ={
+    name:"t-shirt",
+    price:70,
+    description:"A T-shirt (also spelled tee shirt, or tee for short) is a style of fabric shirt named after the T shape of its body and sleeves. Traditionally, it has short sleeves and a round neckline, known as a crew neck, which lacks a collar.",
+    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1HtRxrxy6_axo2X63p8c_2XPVEP7YB_10Mg&s"
+}
+export default product
